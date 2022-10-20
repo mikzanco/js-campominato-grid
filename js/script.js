a2e3c177ca51440e083992d7cc54021f31cb3bc3
mikzanco <mikyzancolo97@gmail.com>
2022-10-20 16:27:07 +0200

    add square in  html and css; start add suqare from js

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// L’utente clicca su un bottone che genererà una griglia di gioco quadrata.
+// Ogni cella ha un numero progressivo, da 1 a 100.
+// Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+// Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+const container = document.querySelector('.container');
+
+const elementsPerRow = 10;
+
+init(elementsPerRow);
+
+function init(numElemets){
+    const totalSquare = Math.pow(numElements, 2);
+
+    for(let i = 0; i < totalSquare; i++){
+        create
+    }
+}
+
