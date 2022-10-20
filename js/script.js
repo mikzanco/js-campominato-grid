ba6905cb9fa5bb25c3537f0618c17cebebe1c0d8
mikzanco <mikyzancolo97@gmail.com>
2022-10-20 17:22:03 +0200

    fix problem che non mi scriveva la classe giusta, avevo messo class name anceh al numero e quindi mi prendeva quello e mi evitava quello prima.

--- js/script.js
+++ js/script.js
@@ -2,27 +2,37 @@
 // Ogni cella ha un numero progressivo, da 1 a 100.
 // Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 // Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+
+
 const container = document.querySelector('.container');
 // creo il bottone che poi mi creerà la griglia
-document.getElementById('start').addEventListener('click', function(){
-    const elementsPerRow = 10;
-    container.innerHTML = '';
-    init(elementsPerRow);
-})
+// document.getElementById('start').addEventListener('click', function(){
+    
+//     container.innerHTML = '';
+    
+// })
 
+const elementsPerRow = 10;
 
+init(elementsPerRow);
 
-
-
-function init(numElemets){
-    const totalSquare = Math.pow(numElements, 2);
-
-    for(let i = 0; i < totalSquare; i++){
+function init(numElements){
+    // faccio il quadrato del numElements con pow per creare il numero giusto di quadrati
+    const totalSquares = Math.pow(numElements, 2);
+    // vado a creare un ciclo per creare poi nella funzione gli square con un conteggio
+    for(let i = 0; i < totalSquares; i++){
         createSquare(i);
     }
 }
 
 function createSquare(idSquare){
     // vado a creare i quadratini e li appendo al container 
+    const square = document.createElement('div');
+    square.className = 'square'; 
+    square.innerHTML = idSquare + 1;
+    square.idElement = idSquare + 1;
+    
+    container.append(square);
 }
+
 
