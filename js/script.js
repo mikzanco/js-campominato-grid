de0469e26fd634524a79b5da697b9ec66b9a304d
mikzanco <mikyzancolo97@gmail.com>
2022-10-20 16:45:10 +0200

    [add] function con la quale creo i quadratini e li appendo al container

--- js/script.js
+++ js/script.js
@@ -3,16 +3,26 @@
 // Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 // Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
 const container = document.querySelector('.container');
+// creo il bottone che poi mi creerà la griglia
+document.getElementById('start').addEventListener('click', function(){
+    const elementsPerRow = 10;
+    container.innerHTML = '';
+    init(elementsPerRow);
+})
 
-const elementsPerRow = 10;
 
-init(elementsPerRow);
+
+
 
 function init(numElemets){
     const totalSquare = Math.pow(numElements, 2);
 
     for(let i = 0; i < totalSquare; i++){
-        create
+        createSquare(i);
     }
+}
+
+function createSquare(idSquare){
+    // vado a creare i quadratini e li appendo al container 
 }
 
